3524132883051b741fbd0773c5c9ffe6711c26f6
Mugammad <mugammadbreda1999@gmail.com>
2024-02-14 19:20:23 +0200

    coming soon page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,17 +8,16 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      Web solutions close to you. 
+      <a target="_blank" rel="noopener">Coming soon...</a>
     </h3>
   </div>
 </template>
 
 <style scoped>
 h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
+  font-weight: 800;
+  font-size: 3rem;
   position: relative;
   top: -10px;
 }
